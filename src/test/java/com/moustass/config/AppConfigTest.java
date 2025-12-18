3c8b5542c2672a9379f30d1c5a7869bf431acddc
Dania Midera <daniamidera@gmail.com>
2025-12-18 23:25:58 +0400

    Initial tree view

---
+++ src/test/java/com/moustass/config/AppConfigTest.java
@@ -0,0 +1,16 @@
+package com.moustass.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppConfigTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getProperty() {
+    }
+}
